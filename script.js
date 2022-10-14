cf59fef548029b293159991aa35f2e8da6255658
Aline Moura <alinesouzamoura_@hotmail.com>
2022-10-14 14:28:08 -0300

    final adjusts

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let priceMeal;
 let priceDrink;
 let priceDessert;
 
-const Options = document.querySelectorAll(".option")
+const Options = document.querySelectorAll(".option");
 Options.forEach((op) => {
-  op.addEventListener("click", select) 
+  op.addEventListener("click", select); 
 })
 
 function select(event) {
@@ -18,24 +18,23 @@ function select(event) {
   if(category === "food") {
     Options.forEach((element) => {
       element.classList.contains("food") && element.classList.remove("active");  
-      addActive(parentDivId, "food")
+      addActive(parentDivId, "food");
     
     })
-  //  meal =  document.querySelector(".food h4").innerHTML
-   meal =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML
-   priceMeal = document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML
-   priceMealNumber = Number(priceMeal.replace(",", ".").split('').slice(3,).join("")) 
+   meal =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML;
+   priceMeal = document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML;
+   priceMealNumber = Number(priceMeal.replace(",", ".").split('').slice(3,).join("")); 
   }
 
   if(category === "drink") {
     Options.forEach((element) => {
       element.classList.contains("drink") && element.classList.remove("active");  
-      addActive(parentDivId)
+      addActive(parentDivId);
       
     })
-   drink =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML
-   priceDrink =  document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML
-   priceDrinkNumber = Number(priceDrink.replace(",", ".").split('').slice(3,).join(""))
+   drink =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML;
+   priceDrink =  document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML;
+   priceDrinkNumber = Number(priceDrink.replace(",", ".").split('').slice(3,).join(""));
   }
 
   if(category === "dessert") {
@@ -44,44 +43,42 @@ function select(event) {
       addActive(parentDivId)
     
     })
-  dessert =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML
-  priceDessert =  document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML
-  priceDessertNumber = Number(priceDessert.replace(",", ".").split('').slice(3,).join(""))     
+  dessert =  document.getElementById(parentDivId).getElementsByTagName("h4")[0].innerHTML;
+  priceDessert =  document.getElementById(parentDivId).getElementsByTagName("h5")[0].innerHTML;
+  priceDessertNumber = Number(priceDessert.replace(",", ".").split('').slice(3,).join(""));    
 }
 
-  addActive(parentDivId)
+  addActive(parentDivId);
 
   openForCheckout();
 }
 
 function addActive (op) {
-  // console.log(meal)
-  document.getElementById(op).classList.add('active')
+  document.getElementById(op).classList.add('active');
 }
 
 function openForCheckout() {
   const actives = document.querySelectorAll(".active");
-  let button = document.getElementById("buttonCheckout")
+  let button = document.getElementById("buttonCheckout");
 
     if(actives.length === 3) {
-        button.classList.add("checkout-available")
-        button.innerHTML = "Fechar pedido"
+        button.classList.add("checkout-available");
+        button.innerHTML = "Fechar pedido";
     }  
 }
 
 function checkout() {
   let message;
-  let total = priceMealNumber + priceDrinkNumber + priceDessertNumber
+  let total = priceMealNumber + priceDrinkNumber + priceDessertNumber;
 
   message = "Olá, gostaria de fazer o pedido: \n - Prato: " + meal 
   + "\n - Bebida: " + drink 
   + "\n - Sobremesa: " + dessert 
   + "\n Total: R$ " + total.toFixed(2);
-  console.log(message)
 
   messageEncoded = window.encodeURIComponent(message); 
 
-  window.open("https://wa.me/5585981828613?text=" + messageEncoded)
+  window.open("https://wa.me/5585981828613?text=" + messageEncoded);
 }
 
 
